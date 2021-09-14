44599db2718ceba8b49821d56f3911333d0620cd
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-14 16:53:26 +0900

    chore: package

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-var fs = require('fs-extra')
+var fs = require('fs-extra');
 
 const sources = ['LICENSE', 'package-lock.json', 'package.json', 'README.md'];
 
@@ -6,7 +6,15 @@ for (const source of sources) {
   fs.copyFileSync(source, `dist/${source}`);
 }
 
-const sourcesInSrc = ['collection.json', 'files'];
+const sourcesInSrc = [
+  'collection.json',
+  'files',
+  'add-jest-to-project/schema.json',
+  'application/schema.json',
+  'convert-karma-to-jest/schema.json',
+  'library/schema.json',
+  'ng-add/schema.json',
+];
 
 for (const source of sourcesInSrc) {
   fs.copySync(`src/${source}`, `dist/${source}`);
